3db9e581c88726096d4853ed9471c90561de600f
家波 卢 <1847096852@qq.com>
2021-08-16 11:13:30 +0800

    更改COM对象创建方式

--- XAJCOMAPP.cpp
+++ XAJCOMAPP.cpp
@@ -13,8 +13,8 @@ int main(int argc, char* argv[])
 	//指向ITEMP类的智能指针
 	IModelPtr pModel = nullptr;
 	//创建 COM 组件对象，有两种方式
-	//HRESULT hr = pModel.CreateInstance(CLSID_Model);     //使用CLSID创建
-	HRESULT hr = pModel.CreateInstance("XAJ.Model");       //使用ProgID创建
+	HRESULT hr = pModel.CreateInstance(CLSID_Model);     //使用CLSID创建
+	//HRESULT hr = pModel.CreateInstance("XAJ.Model");       //使用ProgID创建
 
 	//调用新安江模型
 	pModel->Run(argv);
